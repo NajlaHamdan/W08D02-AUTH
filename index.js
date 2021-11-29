a0cbe8f40b74c9967a52f1cc5b9f87a38639d1f2
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-11-29 16:45:41 +0300

    startup code for server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+// const db = require("./db/db");
+const cors=require("cors");
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+// const registerRouter = require("./router/route/register");
+// app.use("/", registerRouter);
+
+// const logInRouter = require("./router/route/login");
+// app.use("/", logInRouter);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
