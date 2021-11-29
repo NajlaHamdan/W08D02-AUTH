4842d8e3ac3892597f3a4272618821de71fadf22
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-11-29 22:31:26 +0300

    salt and token

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -1,10 +1,17 @@
 const userModel = require("./../../db/models/user");
-const register = (req, res) => {
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const salt = Number(process.env.SALT);
+const secret = process.env.SECRETKEY;
+const register = async (req, res) => {
+
   const { email, password, role } = req.body;
+  const hashed = await bcrypt.hash(password, salt);
   const newUser = new userModel({
     role,
     email,
-    password,
+    password: hashed,
   });
   newUser
     .save()
@@ -16,20 +23,29 @@ const register = (req, res) => {
     });
 };
 const login = (req, res) => {
-  const {  email, password } = req.body;
+  const { email, password } = req.body;
   userModel
-    .findOne({ email })//with find will return email and say not valid if it is valid
-    .then((result) => {
+    .findOne({ email }) //with find will return email and say not valid if it is valid
+    .then(async (result) => {
       if (result) {
         if (result.email == email) {
-          if (result.password == password) {
+          const payload = {
+            role: result.role,
+          };
+          const options = {
+            expiresIn: "60m",
+          };
+          const token = await jwt.sign(payload, secret, options);
+        //   console.log(token);
+          const decrybtedPass = await bcrypt.compare(password, result.password);
+          if (decrybtedPass) {
             res.status("200").json(result);
           } else {
             //   console.log("hi");
             res.status("404").json("email or password is not valid");
           }
         } else {
-            // console.log("hi");
+          // console.log("hi");
           res.status("404").json("email or password is not valid");
         }
       } else {
